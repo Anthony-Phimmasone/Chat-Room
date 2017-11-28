ae346b7cc4095afab8a71d68074eafa05008876b
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2017-11-28 17:12:10 -0500

    Add files via upload

--- November 20 - Anthony Phimmasone/groupProject/index.php
+++ November 20 - Anthony Phimmasone/groupProject/index.php
@@ -1,22 +1,60 @@
- <?php
- ?>
+<?php
 
- <html>
-  <head>
-   <title>Chat Room</title>
+session_start();
 
-	<script
+if(!isset($_SESSION['username'])) {
+?>
+<form name="form2" action="login.php" method="post">
+<table border="1" align="center">
+ <link rel="stylesheet" href="chat.css">
+<div class="header">
+<h1></h1>
+</div>
+
+<tr>
+<td>Username: </td>
+<td><input type="text" name="username"></td>
+</tr>
+
+<tr>
+<td>Password: </td>
+<td><input type="password" name="password"></td>
+</tr>
+
+<tr>
+<td colspan="2"><input type="submit" name="submit" value="Login"></td>
+</tr>
+
+<tr>
+<td colspan="2"><a href="register.php">Create an account</a></td>
+</tr>
+
+</table>
+</form>
+
+
+<?php
+exit;
+}
+
+?>
+
+<html>
+<head>
+<title>Chat Room</title>
+
+<script
   src="jquery-3.2.1.min.js"
-  </script>
+</script>
 
 <script>
+
 function submitChat() {
-	if(form.username.value == '' || form.message.value == '') {
-		alert("All Fields Are Required!");
+	if(form1.msg.value == '') {
+		alert("Enter your message!!!");
 		return;
 	}
-	var username = form.username.value;
-	var message = form.message.value;
+	var msg = form1.msg.value;
 	var xmlhttp = new XMLHttpRequest();
 	
 	xmlhttp.onreadystatechange = function() {
@@ -25,31 +63,34 @@ function submitChat() {
 		}
 	}
 	
-	xmlhttp.open('GET','insert.php?username='+uname+'&message='+msg,true);
+	xmlhttp.open('GET','insert.php?msg='+msg,true);
 	xmlhttp.send();
+
 }
+
 $(document).ready(function(e){
 	$.ajaxSetup({
 		cache: false
 	});
 	setInterval( function(){ $('#chatlogs').load('logs.php'); }, 2000 );
 });
+
 </script>
 
 
-  </head>
-   <body>
-   
-    <form name="form">
-     Enter Your Name: <input type="text" name="username" /> <br />
-     Your Message: <br />
-    <textarea name="message"></textarea><br />
+</head>
+<body>
+<form name="form1">
+Your Chatname: <b><?php echo $_SESSION['username']; ?></b> <br />
+Your Message: <br />
+<textarea name="msg"></textarea><br />
+<a href="#" onclick="submitChat()">Send</a><br /><br />
 
-    <a href="#" onclick="submitChat()">Send</a><br /><br />
-    </form>
-   
-     <div id="chatlogs">
-      LOADING CHAT...
-     </div>
+<a href="logout.php">Logout</a><br /><br />
 
-   </body>
+</form>
+<div id="chatlogs">
+LOADING CHATLOG...
+</div>
+
+</body>
